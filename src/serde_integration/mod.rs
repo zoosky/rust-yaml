b70f39934d480ab5731742f2cba33f8c11971894
Elio S. Jr <elioseverojunior@gmail.com>
2026-05-22 21:50:11 -0300

    feat(serde): re-export from_str/to_string family at crate root

--- src/serde_integration/mod.rs
+++ src/serde_integration/mod.rs
@@ -14,9 +14,7 @@ mod ser;
 #[cfg(feature = "serde")]
 mod value;
 
-// TODO(#21 Task 13): Uncomment when public API lands.
-// These will be uncommented after the implementing functions are added.
-// #[cfg(feature = "serde")]
-// pub use de::{from_reader, from_slice, from_str};
-// #[cfg(feature = "serde")]
-// pub use ser::{to_string, to_writer};
+#[cfg(feature = "serde")]
+pub use de::{from_reader, from_slice, from_str};
+#[cfg(feature = "serde")]
+pub use ser::{to_string, to_writer};
